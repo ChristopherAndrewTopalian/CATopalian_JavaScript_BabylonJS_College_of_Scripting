80e06a480658535d1f31f8239c9b6fba7d9f5029
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-10-19 13:44:12 -0400

    update

--- CATopalian_JavaScript_BabylonJS_College_of_Scripting/src/js/3data/botany.js
+++ CATopalian_JavaScript_BabylonJS_College_of_Scripting/src/js/3data/botany.js
@@ -3,12 +3,22 @@
 let botany = [
 {
     name: "Botany",
-    appURL: "",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
     codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
     description: "Botany",
     textureOffline: 'src/media/textures/botany/titles/botany_title.png',
     // videoURL: ''
 },
+
+{
+    name: "circle_drawn_on_cardboard_box",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "Cardboard box with Circle Ready to be Cut out for the Net Pot",
+    textureOffline: 'src/media/textures/botany/cardboard_box/circle_drawn_on_cardboard_box.png',
+    // videoURL: ''
+},
+
 ];
 
 //----//
