e365a9eb6372a468883ba3d51cea803b05c62711
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-10-19 10:32:24 -0400

    update

--- CATopalian_JavaScript_BabylonJS_College_of_Scripting/src/js/3data/botany.js
+++ CATopalian_JavaScript_BabylonJS_College_of_Scripting/src/js/3data/botany.js
@@ -10,3 +10,12 @@ let botany = [
     // videoURL: ''
 },
 ];
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
