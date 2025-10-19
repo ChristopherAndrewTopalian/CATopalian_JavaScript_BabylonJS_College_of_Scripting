31383fc61e398a42872139568809f39798c88af0
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-10-19 10:31:40 -0400

    update

---
+++ CATopalian_JavaScript_BabylonJS_College_of_Scripting/src/js/3data/botany.js
@@ -0,0 +1,12 @@
+// botany.js
+
+let botany = [
+{
+    name: "Botany",
+    appURL: "",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "Botany",
+    textureOffline: 'src/media/textures/botany/titles/botany_title.png',
+    // videoURL: ''
+},
+];
