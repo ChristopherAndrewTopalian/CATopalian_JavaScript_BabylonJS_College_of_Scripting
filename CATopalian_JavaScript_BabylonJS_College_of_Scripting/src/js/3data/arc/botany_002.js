838157d94db18364a88076c1e38cd4206d2d9225
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-11-28 11:59:01 -0500

    update

---
+++ CATopalian_JavaScript_BabylonJS_College_of_Scripting/src/js/3data/arc/botany_002.js
@@ -0,0 +1,64 @@
+
+
+{
+    name: "we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_002",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "We Utilize a TALL CYLINDER to Minimize the Amount of Material Needed for the Plant to Grow Tall.",     textureOffline: 'src/media/textures/botany/pots/tall_cylinder/we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_002.png',
+    // videoURL: ''
+},
+
+{
+    name: "we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_003",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "We Utilize a TALL CYLINDER to Minimize the Amount of Material Needed for the Plant to Grow Tall.",     textureOffline: 'src/media/textures/botany/pots/tall_cylinder/we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_003.png',
+    // videoURL: ''
+},
+
+{
+    name: "we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_004",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "We Utilize a TALL CYLINDER to Minimize the Amount of Material Needed for the Plant to Grow Tall.",     textureOffline: 'src/media/textures/botany/pots/tall_cylinder/we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_004.png',
+    // videoURL: ''
+},
+
+{
+    name: "we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_005",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "We Utilize a TALL CYLINDER to Minimize the Amount of Material Needed for the Plant to Grow Tall.",     textureOffline: 'src/media/textures/botany/pots/tall_cylinder/we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material_005.png',
+    // videoURL: ''
+},
+
+{
+    name: "we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "We Utilize a TALL CYLINDER to Minimize the Amount of Material Needed for the Plant to Grow Tall.",     textureOffline: 'src/media/textures/botany/pots/tall_cylinder/we_utilize_a_tall_cylinder_for_tall_plant_growth_using_less_material.png',
+    // videoURL: ''
+},
+
+
+
+{
+    name: "coconut_husk_340g_to_fill_a_22_inch_by_1_and_a_half_inch_diameter_cylinder",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_Organic_Hydroponics',
+    description: "340 Grams of Coconut Husk Solidly Fills a 22” Long by 1 ½” Diameter Cylinder. This system uses 95% less growing medium, water and fertilizer, compared to using a 5 gallon pot.",
+    textureOffline: 'src/media/textures/botany/pots/tall_cylinder/coconut_husk_340g_to_fill_a_22_inch_by_1_and_a_half_inch_diameter_cylinder.png',
+    // videoURL: ''
+},
+
+
+];
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
