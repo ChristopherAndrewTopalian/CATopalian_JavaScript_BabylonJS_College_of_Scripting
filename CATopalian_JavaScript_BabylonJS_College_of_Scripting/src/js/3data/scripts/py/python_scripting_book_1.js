1396b81a9a1caea64020b287c8c1b73e23b62f0b
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-01-05 17:04:19 -0500

    update

---
+++ CATopalian_JavaScript_BabylonJS_College_of_Scripting/src/js/3data/scripts/py/python_scripting_book_1.js
@@ -0,0 +1,22 @@
+// thin_wiretronics.js
+
+let python_scripting_book_1 = [
+{
+    name: "print",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_BabylonJS_College_of_Scripting",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_BabylonJS_College_of_Scripting',
+    description: "Python",
+    textureOffline: 'src/media/textures/scripts/py/python_scripting_book_1/print/print.png',
+    // videoURL: ''
+},
+
+{
+    name: "print_with_line_break",
+    appURL: "https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_BabylonJS_College_of_Scripting",
+    codeURL: 'https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_BabylonJS_College_of_Scripting',
+    description: "Python",
+    textureOffline: 'src/media/textures/scripts/py/python_scripting_book_1/print/print_with_line_break.png',
+    // videoURL: ''
+},
+
+];
